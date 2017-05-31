d0f5298287df2c2b09fb7af35ddf0f4addb75aa6
Stanislav Dobrovolschi <st.dobrovolschi@gmail.com>
2017-05-31 19:59:19 +0300

    Added Spring Boot Application class

---
+++ src/main/java/com/sdobrovolschi/hibernate/Application.java
@@ -0,0 +1,12 @@
+package com.sdobrovolschi.hibernate;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author Stanislav Dobrovolschi
+ */
+@SpringBootApplication
+@ComponentScan
+public class Application {
+}
